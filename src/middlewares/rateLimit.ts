93f805bf54b3d1adfe9e46ce96e2e41956e315a6
samzi <salihukoko40@gmail.com>
2025-01-06 17:18:11 +0100

    message updated

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -5,6 +5,6 @@ export const limiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: "draft-7", // Set `RateLimit` and `RateLimit-Policy`` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message:
-    "Too many requests, please try again after an hour.",
+  message: {"message": "Too many requests, please try again after an hour."}
+   ,
 });
