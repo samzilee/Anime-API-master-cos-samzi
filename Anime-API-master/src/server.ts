fd3f889bc93bebd609745a7cb429f54bead705e6
samzi <salihukoko40@gmail.com>
2025-01-06 04:32:07 +0100

    update3

--- Anime-API-master/src/server.ts
+++ Anime-API-master/src/server.ts
@@ -2,15 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 import { limiter } from "./middlewares/rateLimit";
 import { router } from "./routes/routes";
-import cors from "cors";
+
 
 config(); // dotenv
 
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 
-// Enable CORS for all routes
-app.use(cors());
 
 //middlewares
 app.use(limiter);
